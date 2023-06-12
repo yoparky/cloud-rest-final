364b5b673f0985d0ffbcf570f0d2bf04c4d1c3eb
Young-Joon Park <yopark@sas.upenn.edu>
2023-06-11 20:07:30 -0700

    Update base URL

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 const { Datastore } = require('@google-cloud/datastore');
 
-const BASE_URL = 'localhost:8080';
+const BASE_URL = 'https://a9-portfolio.wl.r.appspot.com';
 const CLIENT_ID = 'JZBeYH3V7nHdytXRUmGwW2LBAM3drz0c';
 const CLIENT_SECRET = '-csAtJQ7PIkfTYik43znDp0-PTRVa6dGqCjuik7btGju6uuowUzCjUz8D5ErrTzf';
 const DOMAIN = 'a9-493final.us.auth0.com';
